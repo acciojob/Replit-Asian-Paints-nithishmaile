1741fa62876947fcd46aeaef8bb7e00ff57fe7d0
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-05 18:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+//your JS code here. If required.//your JS code here. If required.
+const formElement=document.getElementById("color-box");
+let allBoxes=document.querySelectorAll(".grid-items")
+
+formElement.addEventListener("submit",(event)=>{
+	event.preventDefault();
+	resetTheColor()
+	const boxId=event.target[0].value;
+	const color=event.target[1].value;
+	const selectedBox=document.getElementById(boxId);
+	selectedBox.style.backgroundColor=color;
+	event.target.reset();
+})
+
+
+function resetTheColor(){
+	for(let box of allBoxes){
+		box.style.backgroundColor="transparent";
+	}
+}
+
